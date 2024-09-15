e0da34e22398170bd37eed8271b3a47b6655e220
Muhammad Abdullah <isiabdullah652@gmail.com>
2024-09-15 11:24:54 +0500

    add new file

---
+++ object/app.js
@@ -0,0 +1,17 @@
+const car = {
+    name: 'civic',
+    model: 'honda',
+    year: '2024',
+    colour: ['red', 'blue', 'black'],
+    drive: function () {
+        console.log('car chal gai he');
+    },
+    stop: function () {
+        console.log('car ruk gai he');
+    },
+
+}
+console.log(car.name);
+console.log(car.model);
+console.log(car.colour[2]);
+console.log(car.stop);
