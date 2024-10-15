8f0f49818bf3261757c08915fefebf982c4b5de7
Muhammad Abdullah <isiabdullah652@gmail.com>
2024-10-15 14:59:27 +0500

    String Method

---
+++ string method/app.js
@@ -0,0 +1,13 @@
+// var fullname = prompt('enter your name')
+// var lastname = prompt('enter your last name')
+// console.log(fullname.toUpperCase() +" "+ lastname.toUpperCase())
+
+
+// var fullname = prompt('enter your name')
+// var lastname = prompt('enter your last name')
+// console.log(fullname.toLowerCase() +" "+ lastname.toLowerCase())
+
+
+
+// var para = 'abdullah iam a front end developer '
+// console.log(para.lastIndexOf('a'))
