c5d576ae3b6965ef334b63ff0d0938770cb9db61
Muhammad Abdullah <isiabdullah652@gmail.com>
2024-10-16 15:15:45 +0500

    basic function

---
+++ chartat ,replace ,math function/app.js
@@ -0,0 +1,29 @@
+// var para ={
+//     class: "matric",
+//     schoolname: "towncommitte",
+//     freinds: ["abdullah","graza","shahzaib"]
+
+// }
+// console.log(para.freinds)
+
+
+// let check = "hello my name is abdullah and iam a front  hello end developer"
+// console.log(check.replaceAll("hello","hy"))
+
+// var user1 = prompt("enter your heads name")
+// var user2 = prompt("enter your tails name")
+// var toss = Math.random() * 2
+// var ch = Math.floor(toss)
+// if(ch == 0){
+//     document.write(user1+ " "+'heads win')
+// }else{
+//     document.write(user2+" "+'tails win')
+// }
+
+// var amount = Math.random() * 20
+// var recover = Math.floor(amount)
+// console.log(recover)
+
+
+// var num = 3152478473
+// console.log(num.toString)
