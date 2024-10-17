dbfe26e652853de3e776878c8d09d3a323091dfa
Muhammad Abdullah <isiabdullah652@gmail.com>
2024-10-17 15:19:57 +0500

    Date Function

---
+++ date time/app.js
@@ -0,0 +1,20 @@
+// date and time method
+
+var date = new Date()
+var day = ['sunday',
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thirsday',
+    'friday',
+    'saturday',
+    'sunday'
+]
+console.log(day[date.getDay()])
+console.log(date.getFullYear())
+console.log(date.getHours())
+console.log(date.getMinutes())
+console.log(date.getMonth())
+console.log(date.getMilliseconds())
+
+
