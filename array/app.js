b7837a1c9c06fc96a9ed3fdb70cf1d65f7f49e60
Muhammad Abdullah <isiabdullah652@gmail.com>
2024-12-04 23:50:23 +0500

    array - method

---
+++ array/app.js
@@ -0,0 +1,19 @@
+
+
+// // arrray method
+
+// let arr = ["abdullah","uzair","raza","huraira"]
+
+// // arr.push("hamza")
+
+
+// // arr.pop()
+
+// // arr.unshift("hamza")
+
+// // arr.shift()
+
+// // arr.splice(3,1 ,"hamza")
+
+// let newarray = arr.slice(0, 2)
+// console.log(newarray);
