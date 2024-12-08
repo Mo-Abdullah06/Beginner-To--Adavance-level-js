1dd762cdf8855c12879b87529379c41120b6604c
Muhammad Abdullah <isiabdullah652@gmail.com>
2024-12-08 23:24:18 +0500

    nested loops

---
+++ nested loops/app.js
@@ -0,0 +1,43 @@
+
+
+// array create
+
+// let arr = ["Abdullah", "shahzaib", "uzair", "raza","ali"]
+// let arr2 = ["10", "numair" ]
+
+
+// for (let i = 0; i < arr.length; i++) {
+
+//     for (let j = 0; j < arr2.length; j++) {
+//         document.write(arr[i] + " " + arr2[j] +"<br>")
+//     }
+// }
+
+
+// document.write(arr)
+
+// for (let i = 0; i < arr.length; i++) {
+
+// document.write(arr[i] + "<br>")
+// }
+
+for (let i = 1; i <= 100; i = i + 10) {
+    for (let j = i; j < i + 10; j++) {
+        document.write(j + " ")
+        // document.write("<br>")
+    }
+    document.write("<br>")
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
