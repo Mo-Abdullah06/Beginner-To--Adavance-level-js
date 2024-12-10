4ca6619df5e612ac597e8d42d78bc94bed7b2f2c
Muhammad Abdullah <isiabdullah652@gmail.com>
2024-12-10 13:49:14 +0500

    for in loop

---
+++ for in loops/app.js
@@ -0,0 +1,16 @@
+// create 
+
+// const obj = {
+
+//     name: "Abdullah",
+//     fahtername: "Nazim",
+//     class: "10",
+//     subject: "english",
+//     age: "19"
+// }
+
+// document.write(obj.name +" "+ obj.age)
+
+// for(let key in obj ){
+//     document.write(key + ":" +obj[key] +"<br>")
+// }
