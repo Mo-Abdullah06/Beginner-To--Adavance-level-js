a3f8effbd57280de925140e64a3438733f2227c2
Muhammad Abdullah <isiabdullah652@gmail.com>
2024-12-13 20:05:22 +0500

    switch statment

---
+++ switch statment/app.js
@@ -0,0 +1,22 @@
+
+
+
+let num = +prompt('enter your value')
+
+switch (num) {
+    case 10:
+        alert("han mil gaya")
+        break
+        
+        case 20:
+            alert("han mil gaya")
+break
+        
+
+    default:
+        alert("nhi mila")
+        break;
+}
+
+
+
