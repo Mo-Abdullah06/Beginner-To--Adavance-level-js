1ab84afca04568cc30aa0cb58fab585ae86a22ed
Muhammad Abdullah <isiabdullah652@gmail.com>
2024-12-15 22:06:40 +0500

    ES5 VAR vs ES6 LET< CONST

---
+++ var let const/app.js
@@ -0,0 +1,61 @@
+
+// ES5 var
+
+
+// var declare
+
+
+var name  = 'abdullah'
+
+// console.log(name);
+
+// re declaration var
+
+var name  = 'nazim'
+
+// console.log(name);
+
+// re assign var 
+
+name = 'hamza'
+// console.log(name);
+
+// ES6
+
+// declare let
+
+let batch = 12
+// console.log(batch);
+
+
+// let not accept re decleration
+
+// let batch = 12
+
+// but let is accept re assign
+
+batch = 13
+// console.log(batch);
+
+
+
+// declare const
+
+
+const subject = 'english'
+// console.log(subject);
+
+// const not accept is re declaration
+// const subject = ""
+
+// const not accept is re assign
+subject = 'english'
+// console.log(subject);
+
+
+
+
+
+
+
+
