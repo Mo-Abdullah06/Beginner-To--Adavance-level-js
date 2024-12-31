116ecf2d143f7a54d271b60225a09ec3aa9e7a93
Muhammad Abdullah <isiabdullah652@gmail.com>
2024-12-31 17:08:44 +0500

    while loop

---
+++ while loop/app.js
@@ -0,0 +1,28 @@
+
+// counting print
+
+// let a = 0;
+
+// while(a <10) {
+//     a++
+
+//     console.log(a);
+// }
+
+
+// let a = 11;
+
+// while(a >1) {
+//     a--
+
+//     console.log(a);
+// }
+
+
+
+// let city = ['karachi', 'islamabad', 'quetta', 'lahore']
+// let i = 0
+// while(i < city.length){
+//     document.write(city[i] , '<br>')
+//     i++
+// }
