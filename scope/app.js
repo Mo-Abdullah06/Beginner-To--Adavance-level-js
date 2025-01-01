97b0a67a0ad98ff765de4bcb4e54e89b9766001d
Muhammad Abdullah <isiabdullah652@gmail.com>
2025-01-01 17:36:47 +0500

    scope local and global

---
+++ scope/app.js
@@ -0,0 +1,45 @@
+
+
+// GLOBAL SCOPE
+
+// var globalscope = 'iam a global '
+
+// function check(){
+//     console.log(globalscope);
+
+// }
+// check();
+
+
+// LOCAL SCOPE
+
+// function localscope(){
+//     let name = 'abdullah'
+//     console.log(name);
+// }
+// localscope();
+
+// let age = 19
+
+// function scope() {
+
+//     let name = 'abdullah'
+
+//     function another() {
+//         let job = 'developer'
+
+
+//         console.log(age);
+//         console.log(name);
+//         console.log(job);
+
+
+//     }
+//     another();
+
+//     console.log(name);
+//     console.log(job);
+
+
+// }
+// scope();
