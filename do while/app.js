d184f85095bf12d10dd06fd8a2e0664b751776d5
Muhammad Abdullah <isiabdullah652@gmail.com>
2025-01-02 12:47:18 +0500

    do while loop

---
+++ do while/app.js
@@ -0,0 +1,24 @@
+
+// let a = 1
+// do{
+//     document.write(a,'<br>')
+//     a++
+// }while(a <= 10)
+
+
+
+
+// document.write(obj)
+
+// let workers = ['abdullah', 'hasnain','ali']
+
+// let i =0
+// do{
+//     document.write(workers[i] , "<br>")
+//     i++
+// }while(i < workers.length)
+
+
+
+
+// document.write(workers)
