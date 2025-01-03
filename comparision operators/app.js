58b3d75fa64ade1291331556d04c3cd6d025cc25
Muhammad Abdullah <isiabdullah652@gmail.com>
2025-01-03 21:22:26 +0500

    comparision operator

---
+++ comparision operators/app.js
@@ -0,0 +1,71 @@
+
+// assign operators =
+
+// let  name = 'abdulllah'
+// console.log(name);
+
+// condition check but not check data type ==
+
+// let num = 5
+// if (num == "5") {
+//     console.log(true);
+    
+    
+// }
+
+// conditon and data type  check  ===
+
+// let num = 5
+// if (num === "5") {
+//     console.log(true);
+// }
+// else{
+//     console.log(false);
+// }
+
+// both side check operator &&
+
+// let num1 = 2
+// let num2 = 5
+
+// if (num1 == 2 && num2 == 4) {
+//     console.log(true);
+    
+// } else {
+//     console.log(false);
+    
+// }
+
+// one side true operator ||
+
+// let num1 = 2
+// let num2 = 5
+
+// if (num1 == 2 || num2 == 4) {
+//     console.log(true);
+    
+// } else {
+//     console.log(false);
+    
+// }
+
+
+// not true opeartor !
+
+// let num1 = 2
+// let num2 = 5
+
+// if (!num1 == 2 && num2 == 4) {
+//     console.log(true);
+    
+// } else {
+//     console.log(false);
+    
+// }
+
+
+// greator than operator >
+// less then operator <
+// gretaor than and equal >= 
+// greator tham and equal operator <=
+
